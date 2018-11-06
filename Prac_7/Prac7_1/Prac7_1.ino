81169b5b3d0a93ed7faafa31c64974acf91a93fb
Migarve55 <migarve55@gmail.com>
2018-11-06 12:50:44 +0100

    Mejorado la 7.1
    Ahora esta bastante completo

--- Prac_7/Prac7_1/Prac7_1.ino
+++ Prac_7/Prac7_1/Prac7_1.ino
@@ -1,6 +1,9 @@
 #include <ArduinoJson.h>
 #include <Ethernet.h> //Importamos librería Ethernet
 
+#define JSON_BUFFER_SIZE 80
+#define LED_PIN 9
+
 byte mac[] = {0x54, 0x55, 0x58, 0x10, 0x00, 0x24};
 
 EthernetServer servidor(80); //Puerto en el 80
@@ -11,14 +14,33 @@ IPAddress ip(192, 168, 61, 204);
 
 //Control del led
 
+bool ledState = false;
+
 void apagar() {
   Serial.println("Apagar led");
-  digitalWrite(led,LOW);
+  ledState = false;
+  digitalWrite(LED_PIN,LOW);
 }
 
 void encender() {
   Serial.println("Encender Led");
-  digitalWrite(led,HIGH);
+  ledState = true;
+  digitalWrite(LED_PIN,HIGH);
+}
+
+void mandarJSON(EthernetClient cliente) {
+  Serial.println("Mandando info de los sensores"); 
+  StaticJsonBuffer<JSON_BUFFER_SIZE> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["temperatura"] = 15.0;
+  root["humedad"] = 0.5;
+  root["led"] = ledState;
+  //Mandar headers y mandar
+  cliente.println("HTTP/1.0 200 OK");
+  cliente.println("Content-Type: application/json");
+  cliente.println("Connection: close");
+  cliente.println();
+  root.prettyPrintTo(cliente);
 }
 
 void setup() {
@@ -30,7 +52,7 @@ void setup() {
    Serial.println("IP Local: " + Ethernet.localIP());
    // Inicializar el led
    pinMode(LED_PIN,OUTPUT);
-   encender();
+   apagar();
 }
 
 void loop(){
@@ -43,24 +65,20 @@ void loop(){
          char c = cliente.read(); //Leer petición carácter a carácter
          peticion.concat(c); // concatenar en un string
          // Ha acabado la peticion http
-         // Si contiene index responder con una web
-         // la petición ha acabado '\n' y contiene la cadena "index"
-         if (c == '\n' && peticion.indexOf("index") != -1){
+         if (c == '\n'){
            Serial.println("Responder");
-           // Serial.println(peticion);
-          
            // contiene la cadena "encender"
-           if(peticion.indexOf("encender") != -1){
+           if(peticion.indexOf("sensor") != -1){
+              mandarJSON(cliente);
+           } else if(peticion.indexOf("ledOn") != -1){
               encender();
-           }else if(peticion.indexOf("apagar") != -1){
-              apagar();           
+              cliente.println("HTTP/1.1 200 OK");
+              cliente.println();
+           } else if(peticion.indexOf("ledOff") != -1){
+              apagar();
+              cliente.println("HTTP/1.1 200 OK");
+              cliente.println();
            }
-          
-           // Enviamos al cliente una respuesta HTTP
-           cliente.println("HTTP/1.1 200 OK");
-           cliente.println("Content-Type: text/html");
-           cliente.println("Access-Control-Allow-Origin: *");
-           cliente.println();
            break;
          }
        }
--- Prac_7/Server/script.js
+++ Prac_7/Server/script.js
@@ -7,48 +7,84 @@ function initMap() {
 
     var contentString = 'Hola que tal, soy un sensor tope de wapo colega';
 
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-
     var markerS1 = new google.maps.Marker({
         position: {lat: 43.354839, lng: -5.851137},
         map: map,
         title: 'Sensor 1'
     });
     
-    markerS1.addListener('click', function() {
-        infowindow.open(map, markerS1);
-    });
-    
     var markerS2 = new google.maps.Marker({
         position: {lat: 43.356091, lng: -5.853795},
         map: map,
-        title: 'Sensor 1'
-    });
-    
-    markerS2.addListener('click', function() {
-        infowindow.open(map, markerS2);
+        title: 'Sensor 2'
     });
     
     var markerS3 = new google.maps.Marker({
         position: {lat: 43.352593, lng: -5.853424},
         map: map,
-        title: 'Sensor 1'
-    });
-    
-    markerS3.addListener('click', function() {
-        infowindow.open(map, markerS3);
+        title: 'Sensor 3'
     });
     
     var markerS4 = new google.maps.Marker({
         position: {lat: 43.353079, lng: -5.850734},
         map: map,
-        title: 'Sensor 1'
+        title: 'Sensor 4'
     });
     
-    markerS4.addListener('click', function() {
-        infowindow.open(map, markerS4);
+    setMarkerPopUp(map, markerS1, getData);
+    setMarkerPopUp(map, markerS2, getRandomData);
+    setMarkerPopUp(map, markerS3, getRandomData);
+    setMarkerPopUp(map, markerS4, getRandomData);
+}
+
+function setMarkerPopUp(map, marker, dataGateway) {
+    marker.addListener('click', function() {
+        var infoWindow = new google.maps.InfoWindow({
+            content: getContentString(dataGateway())
+         });
+        infoWindow.open(map, marker);
     });
-    
+}
+
+function getContentString(data) {
+    return `
+        <p>Datos del sensor: </p>
+        <p>Temperatura: ${data.temperatura.toFixed(2)}ºC</p>
+        <p>Humedad: ${data.humedad.toFixed(2)}%</p>
+        <button onclick="changeLed()">${getButtonText(data.led)}</button>
+    `;
+}
+
+function getRandomData() {
+    return {
+        temperatura: Math.random() * 30,
+        humedad: Math.random()
+    }
+}
+
+function getButtonText(estado) {
+    if (estado == true) 
+        return "Apagar";
+    else
+        return "Encender";
+}
+
+function getData() {
+    result = {
+        temperatura: 0,
+        humedad: 0,
+        led: false
+    };
+    $.ajax({
+        dataType: "json",
+        url: "http://192.168.61.204/sensor",
+        async: false,
+        success: function(data) {
+            result = data;
+        },
+        error: function() {
+            alert("No se ha podido recibir información del sensor");
+        }
+    });
+    return result;
 }
