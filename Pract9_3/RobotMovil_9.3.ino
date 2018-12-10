f82373da8a5fa94be1bf45c8bef69e0c90ad26fc
AlfaBlaster <43543401+AlfaBlaster@users.noreply.github.com>
2018-12-10 17:50:19 +0100

    Practica 9.3

---
+++ Pract9_3/RobotMovil_9.3.ino
@@ -0,0 +1,159 @@
+#include <Servo.h>
+
+Servo servoIzq;
+Servo servoDer;
+
+int PIN_TRIG = 0;
+int PIN_ECHO = 0;
+
+int NO_LINEA = LOW; 
+int LINEA = HIGH;
+
+int pinServoIzq = 8;
+int pinServoDer = 9;
+
+int pinInfraRojoIzq = 2;
+int pinInfraRojoDer = 3;
+
+int numLoop = 0;
+int timeNow = 0;
+
+long xTiempo = 1000;
+
+int iz = 1;
+int der = 0;
+
+int THRESHOLD = 70;
+
+void setup(){
+ Serial.begin(9600); // Descomentar si queréis debuguear 
+ numLoop = 0;
+ pinMode(pinInfraRojoDer, INPUT);
+ pinMode(pinInfraRojoIzq, INPUT);
+ pinMode(PIN_TRIG, OUTPUT);
+ pinMode(PIN_ECHO, INPUT);  
+ servoIzq.attach(pinServoIzq);
+ servoDer.attach(pinServoDer);
+}
+void loop(){
+  if(isSomething()){
+    entrarEnPista()
+  }
+   //No detecta linea, busca
+  else if(digitalRead(pinInfraRojoIzq) == NO_LINEA && digitalRead(pinInfraRojoDer) == NO_LINEA){
+    
+    
+    entrarEnPista();
+  } 
+  //Linea recta
+  else  if(digitalRead(pinInfraRojoIzq) == LINEA && digitalRead(pinInfraRojoDer) == LINEA){
+        avanzar();
+  }
+   //Curva detectada 
+  else if(digitalRead(pinInfraRojoIzq) == LINEA || digitalRead(pinInfraRojoDer) == LINEA){
+    if(numLoop>0){
+      servoIzq.write(90+numLoop);
+      servoDer.write(0);
+      delay(50);
+        }   
+         corregirTrayectoria();
+  }
+ }
+
+  void  entrarEnPista(){
+
+    if(iz == 1){
+      espiralIzquierda();
+    }else{
+      espiralDerecha();
+    }      
+  }
+
+ 
+  void corregirTrayectoria(){
+    //pasitoParaAtras();
+     numLoop = 0;
+    //linea detectada  a la derecha  
+    if(digitalRead(pinInfraRojoIzq) == NO_LINEA && digitalRead(pinInfraRojoDer) == LINEA){
+      servoIzq.write(0);
+      servoDer.write(90);
+      iz= 0;
+      der = 1;
+    }
+    //linea detectada  a la izquierda  
+    else  if(digitalRead(pinInfraRojoIzq) == LINEA && digitalRead(pinInfraRojoDer) == NO_LINEA){
+      servoIzq.write(90);
+      servoDer.write(180);
+       iz= 0;
+       der = 1;
+    }
+    numLoop = 0;
+    delay(150); // Media vuelta
+ }
+ 
+ void avanzar(){
+    //pasitoParaAtras();
+    numLoop = 0;
+    servoIzq.write(0);
+    servoDer.write(180);
+    numLoop = 0;
+     iz= 1;
+     der = 0;
+    delay(150); // Media vuelta
+   }
+   void incLoop(){
+    if(millis()-timeNow > xTiempo){
+      Serial.println(millis()-timeNow);
+      
+      if(numLoop<60 ){
+        numLoop++;
+    }
+    timeNow = millis();
+   }
+   }
+   void espiralIzquierda(){
+    incLoop();
+      servoIzq.write(90-numLoop);
+      servoDer.write(180);
+      delay(100);
+      Serial.println(90-numLoop);
+   }
+
+   void espiralDerecha(){
+    incLoop();
+      servoIzq.write(0);
+      servoDer.write(90+numLoop);
+      delay(100);
+      Serial.println(90+numLoop);
+
+   }
+   void pasitoParaAtras(){
+    if(numLoop>0){
+      servoIzq.write(180);
+      servoDer.write(0);
+      delay(50);
+    }
+   }
+
+ void esquivar(){
+      servoIzq.write(90);
+      servoDer.write(180);
+      
+ }
+
+ boolean isSomething(){
+  return detectedDistance() < THRESHOLD;
+ }
+ long detectedDistance (){
+  //Medir distancia
+  digitalWrite(PIN_TRIG, LOW);
+  delayMicroseconds(5);
+
+  digitalWrite(PIN_TRIG, HIGH); /* Emitimos el pulso ultrasónico */
+  delayMicroseconds(10);
+
+  long responseTime = pulseIn(PIN_ECHO, HIGH);
+
+  return long(0.01716*responseTime);
+}
+ 
